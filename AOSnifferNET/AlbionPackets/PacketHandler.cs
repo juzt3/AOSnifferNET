c7098b0f3f68136bb30bbd333ba01b6af33bf222
Felipe Gatica (Paragati Das) <juzt3@users.noreply.github.com>
2026-04-13 21:05:23 -0400

    join change

--- AOSnifferNET/AlbionPackets/PacketHandler.cs
+++ AOSnifferNET/AlbionPackets/PacketHandler.cs
@@ -1078,7 +1078,7 @@ namespace AOSnifferNET
             int currentEnergy = int.Parse(parameters[16].ToString());
             int maxEnergy = int.Parse(parameters[17].ToString());
 
-            int faction = int.Parse(parameters[48].ToString());
+            int faction = int.Parse(parameters[49].ToString());
 
             var jo = new OpJoin(cId, markId, cName, cluster, pos, angle, currentHealth, maxHealth, currentEnergy, maxEnergy, faction);
             printOperationInfo(jo, OperationCodes.Join, "onResponse");
